68aee271eb015417d81e81e023164e463412a423
Sebastiaan <sebastiaan@avido.nl>
2021-03-18 09:43:01 +0100

    fix for entity id

--- tests/Feature/Endpoints/StatusTest.php
+++ tests/Feature/Endpoints/StatusTest.php
@@ -20,7 +20,7 @@ class StatusTest extends TestCase
         $status = $this->client->status->get(1);
         $this->assertInstanceOf(ProcessStatus::class, $status);
         $this->assertSame(1, $status->id);
-        $this->assertSame(555551, $status->entityId);
+        $this->assertSame('555551', $status->entityId);
         $this->assertSame('CONFIRM_SHIPMENT', $status->eventType);
         $this->assertSame('PENDING', $status->status);
     }
@@ -43,7 +43,7 @@ class StatusTest extends TestCase
         $status = $this->client->status->get($processStatus);
         $this->assertInstanceOf(ProcessStatus::class, $status);
         $this->assertSame(1, $status->id);
-        $this->assertSame(555551, $status->entityId);
+        $this->assertSame('555551', $status->entityId);
         $this->assertSame('CONFIRM_SHIPMENT', $status->eventType);
         $this->assertSame('PENDING', $status->status);
     }
@@ -54,7 +54,7 @@ class StatusTest extends TestCase
         $status = $this->client->status->getByEntityId(555551, 'CONFIRM_SHIPMENT');
         $this->assertInstanceOf(ProcessStatusCollection::class, $status);
         $this->assertSame(1, $status->processStatuses->first()->id);
-        $this->assertSame(555551, $status->processStatuses->first()->entityId);
+        $this->assertSame('555551', $status->processStatuses->first()->entityId);
         $this->assertSame('CONFIRM_SHIPMENT', $status->processStatuses->first()->eventType);
         $this->assertSame('PENDING', $status->processStatuses->first()->status);
     }
@@ -66,7 +66,7 @@ class StatusTest extends TestCase
         $this->expectExceptionMessage('Validation Failed, See violations');
 
         try {
-            $this->client->status->getByEntityId(555551, 'INVALID_EVENT_TYPE');
+            $this->client->status->getByEntityId('555551', 'INVALID_EVENT_TYPE');
         } catch (ValidationException $e) {
             $violations = $e->getViolations();
             $this->assertCount(1, $violations);
@@ -84,12 +84,12 @@ class StatusTest extends TestCase
         $this->assertInstanceOf(ProcessStatusCollection::class, $status);
         $this->assertCount(2, $status->processStatuses);
         $this->assertSame(1, $status->processStatuses->first()->id);
-        $this->assertSame(555551, $status->processStatuses->first()->entityId);
+        $this->assertSame('555551', $status->processStatuses->first()->entityId);
         $this->assertSame('CONFIRM_SHIPMENT', $status->processStatuses->first()->eventType);
         $this->assertSame('PENDING', $status->processStatuses->first()->status);
 
         $this->assertSame(2, $status->processStatuses->last()->id);
-        $this->assertSame(555552, $status->processStatuses->last()->entityId);
+        $this->assertSame('555552', $status->processStatuses->last()->entityId);
         $this->assertSame('CANCEL_ORDER', $status->processStatuses->last()->eventType);
         $this->assertSame('SUCCESS', $status->processStatuses->last()->status);
     }
@@ -103,12 +103,12 @@ class StatusTest extends TestCase
         $this->assertInstanceOf(ProcessStatusCollection::class, $status);
         $this->assertCount(2, $status->processStatuses);
         $this->assertSame(1, $status->processStatuses->first()->id);
-        $this->assertSame(555551, $status->processStatuses->first()->entityId);
+        $this->assertSame('555551', $status->processStatuses->first()->entityId);
         $this->assertSame('CONFIRM_SHIPMENT', $status->processStatuses->first()->eventType);
         $this->assertSame('PENDING', $status->processStatuses->first()->status);
 
         $this->assertSame(2, $status->processStatuses->last()->id);
-        $this->assertSame(555552, $status->processStatuses->last()->entityId);
+        $this->assertSame('555552', $status->processStatuses->last()->entityId);
         $this->assertSame('CANCEL_ORDER', $status->processStatuses->last()->eventType);
         $this->assertSame('SUCCESS', $status->processStatuses->last()->status);
     }
