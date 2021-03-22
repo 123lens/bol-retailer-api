2efd2ef7b3d344fdbfb4840d0108c4fb8fdd370d
Sebastiaan <sebastiaan@avido.nl>
2021-03-22 09:24:54 +0100

    Added mock files for status

--- tests/Feature/Endpoints/StatusTest.php
+++ tests/Feature/Endpoints/StatusTest.php
@@ -20,6 +20,8 @@ class StatusTest extends TestCase
     /** @test */
     public function waitForStatusToComplete()
     {
+        $this->useMock('200-status-success-response.json');
+
         $status = $this->client->status->waitUntilComplete(2, 2, 3);
         $this->assertInstanceOf(ProcessStatus::class, $status);
         $this->assertSame(2, $status->id);
@@ -31,6 +33,8 @@ class StatusTest extends TestCase
     /** @test */
     public function getStatusById()
     {
+        $this->useMock('200-status-get-status-by-id.json');
+
         $status = $this->client->status->get(1);
         $this->assertInstanceOf(ProcessStatus::class, $status);
         $this->assertSame(1, $status->id);
@@ -42,6 +46,8 @@ class StatusTest extends TestCase
     /** @test */
     public function statusNotFoundException()
     {
+        $this->useMock('404-status-not-found.json', 404);
+
         $this->expectException(BolRetailerException::class);
         $this->expectExceptionMessage('Error executing API call : Not Found : Not Found (404)');
         $status = $this->client->status->get(99999);
@@ -50,6 +56,8 @@ class StatusTest extends TestCase
     /** @test */
     public function getStatusByResource()
     {
+        $this->useMock('200-status-get-status-by-resource.json');
+
         $processStatus = new ProcessStatus([
             'id' => 1
         ]);
@@ -65,6 +73,8 @@ class StatusTest extends TestCase
     /** @test */
     public function getStatusByEntityId()
     {
+        $this->useMock('200-status-get-status-by-entity-id.json');
+
         $status = $this->client->status->getByEntityId(555551, 'CONFIRM_SHIPMENT');
         $this->assertInstanceOf(ProcessStatusCollection::class, $status);
         $this->assertSame(1, $status->processStatuses->first()->id);
@@ -76,6 +86,8 @@ class StatusTest extends TestCase
     /** @test */
     public function getStatusByInvalidEventTypeThrowsAnException()
     {
+        $this->useMock('400-status-invalid-event-type.json', 400);
+
         $this->expectException(ValidationException::class);
         $this->expectExceptionMessage('Validation Failed, See violations');
 
@@ -92,6 +104,8 @@ class StatusTest extends TestCase
     /** @test */
     public function getMultipleProcessStatusUsingBatch()
     {
+        $this->useMock('200-status-multiple-processes.json');
+
         $batch = [1,2];
 
         $status = $this->client->status->batch($batch);
@@ -111,6 +125,8 @@ class StatusTest extends TestCase
     /** @test */
     public function getMultipleProcessStatusUsingBatchWithCrazyMixup()
     {
+        $this->useMock('200-status-multiple-processes.json');
+
         $batch = [1,new ProcessStatus(['id' => 2])];
 
         $status = $this->client->status->batch($batch);
