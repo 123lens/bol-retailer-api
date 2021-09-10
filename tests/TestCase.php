f3877e496310a93ecb549c02305428fa3c6064bd
Sebastiaan <sebastiaan@avido.nl>
2021-09-10 13:50:29 +0200

    Added example file

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -1,11 +1,7 @@
 <?php
 namespace Budgetlens\BolRetailerApi\Tests;
 
-use Budgetlens\BolRetailerApi\ApiConfig;
 use Budgetlens\BolRetailerApi\Contracts\Config;
-use Dotenv\Dotenv;
-use Dotenv\Exception\InvalidFileException;
-use Dotenv\Exception\InvalidPathException;
 use Budgetlens\BolRetailerApi\Client;
 use GuzzleHttp\Handler\MockHandler;
 use GuzzleHttp\Psr7\Response;
@@ -26,14 +22,6 @@ abstract class TestCase extends BaseTestCase
 
     protected function setUp(): void
     {
-        try {
-            (Dotenv::createUnsafeImmutable(__DIR__.'/..'))->load();
-        } catch (InvalidPathException $e) {
-            //
-        } catch (InvalidFileException $e) {
-            exit('The environment file is invalid: '.$e->getMessage());
-        }
-
         $this->client = new Client(new TestApiConfig());
 
         parent::setUp();
