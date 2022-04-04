4a854cce800b21de5b5c7e09964229d909d7568e
Sebastiaan Blaas <sebastiaan@avido.nl>
2022-04-04 11:16:31 +0200

    Register endpoint

--- src/SharedClient.php
+++ src/SharedClient.php
@@ -1,15 +1,21 @@
 <?php
 namespace Budgetlens\BolRetailerApi;
 
+use Budgetlens\BolRetailerApi\Endpoints\Status;
+
 class SharedClient extends BaseClient
 {
     protected $endpoint = 'https://api.bol.com/shared';
     protected $endpointTest = 'https://api.bol.com/shared-demo';
+
+    /** @var  Status */
+    public $status;
 
     /**
      * Initialize available endpoints
      */
     public function initializeEndpoints(): void
     {
+        $this->status = new Status($this);
     }
 }
