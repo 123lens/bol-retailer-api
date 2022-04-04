c7513e6d1abaa23ec21180a9e2b58b470a316aa3
Sebastiaan Blaas <sebastiaan@avido.nl>
2022-04-04 09:39:53 +0200

    Placeholder advertising client

---
+++ src/AdvertisingClient.php
@@ -0,0 +1,12 @@
+<?php
+namespace Budgetlens\BolRetailerApi;
+
+class AdvertisingClient extends BaseClient
+{
+    /**
+     * Initialize available endpoints
+     */
+    public function initializeEndpoints(): void
+    {
+    }
+}
