677a902bfc142ba2201a1be522881d677130237a
Sebastiaan Blaas <sebastiaan@avido.nl>
2022-04-05 12:07:36 +0200

    Added promotions endpoint

---
+++ src/Endpoints/Promotions.php
@@ -0,0 +1,86 @@
+<?php
+namespace Budgetlens\BolRetailerApi\Endpoints;
+
+use Budgetlens\BolRetailerApi\Resources\Commission;
+use Budgetlens\BolRetailerApi\Resources\Promotion;
+use Budgetlens\BolRetailerApi\Resources\Promotions\Product;
+use Illuminate\Support\Collection;
+
+class Promotions extends BaseEndpoint
+{
+    /**
+     * Get Promotions
+     *
+     * @see https://api.bol.com/retailer/public/redoc/v7/retailer.html#operation/get-promotions
+     * @param string|null $promotionType
+     * @param int $page
+     * @return Collection
+     */
+    public function list(string $promotionType = 'AWARENESS', int $page = 1): Collection
+    {
+        $parameters = collect([
+            'promotion-type' => $promotionType,
+            'page' => $page,
+        ])->reject(function ($value) {
+            return empty($value);
+        });
+
+        $response = $this->performApiCall(
+            'GET',
+            "promotions" . $this->buildQueryString($parameters->all())
+        );
+
+        $collection = new Collection();
+
+        collect($response->promotions)->each(function ($item) use ($collection) {
+            $collection->push(new Promotion($item));
+        });
+
+        return $collection;
+    }
+
+    /**
+     * Get Promotion By ID
+     * @see https://api.bol.com/retailer/public/redoc/v7/retailer.html#operation/get-promotion
+     * @param string $id
+     * @return Promotion
+     */
+    public function get(string $id): Promotion
+    {
+        $response = $this->performApiCall(
+            'GET',
+            "promotions/{$id}"
+        );
+
+        return new Promotion(collect($response));
+    }
+
+    /**
+     * Gets a paginated list of all products that are present within a promotion.
+     * @see https://api.bol.com/retailer/public/redoc/v7/retailer.html#operation/get-products
+     * @param string $id
+     * @param $page
+     * @return Collection
+     */
+    public function products(string $id, $page = 1)
+    {
+        $parameters = collect([
+            'page' => $page,
+        ])->reject(function ($value) {
+            return empty($value);
+        });
+
+        $response = $this->performApiCall(
+            'GET',
+            "promotions/{$id}/products" . $this->buildQueryString($parameters->all())
+        );
+
+        $collection = new Collection();
+
+        collect($response->promotions)->each(function ($item) use ($collection) {
+            $collection->push(new Product($item));
+        });
+
+        return $collection;
+    }
+}
---
+++ src/Resources/Country.php
@@ -0,0 +1,10 @@
+<?php
+namespace Budgetlens\BolRetailerApi\Resources;
+
+class Country extends BaseResource
+{
+    public $countryCode;
+    public $value;
+    public $minimum;
+    public $maximum;
+}
