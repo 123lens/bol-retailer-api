7c7122c93233d26170dff0866b5b95cc899c3550
Sebastiaan Blaas <sebastiaan@avido.nl>
2022-10-05 15:19:56 +0200

    Refactored replenishments, added getProductDestinations

---
+++ src/Resources/ProductDestination.php
@@ -0,0 +1,30 @@
+<?php
+namespace Budgetlens\BolRetailerApi\Resources;
+
+use Budgetlens\BolRetailerApi\Resources\Replenishment\Warehouse;
+
+class ProductDestination extends BaseResource
+{
+    public $destinationWarehouse;
+    public $eans;
+
+    public function setDestinationWarehouseAttribute($value): self
+    {
+        if (!$value instanceof Warehouse) {
+            $value = new Warehouse($value->address);
+        }
+
+        $this->destinationWarehouse = $value;
+
+        return $this;
+    }
+
+//    public function toArray(): array
+//    {
+//        return collect(parent::toArray())
+//            ->reject(function ($value) {
+//                return empty($value);
+//            })
+//            ->all();
+//    }
+}
