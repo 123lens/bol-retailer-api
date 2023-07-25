c18ce43d8d0befc8b0e266b1c1e6ca459cf47d1f
Sebastiaan Blaas <sebastiaan@avido.nl>
2023-07-25 16:01:28 +0200

    public function getProductsListWithInvalidFilterRange()     {-        $this->expectException(ValidationException::class);+        $this->expectException(ValidationException::class);          $request = new ListProductsRequest([             'countryCode' => 'BE',Add tests for invalid filter ranges in ProductsTest

--- tests/Feature/Endpoints/RetailerApi/ProductsTest.php
+++ tests/Feature/Endpoints/RetailerApi/ProductsTest.php
@@ -1,6 +1,7 @@
 <?php
 namespace Budgetlens\BolRetailerApi\Tests\Feature\Endpoints\RetailerApi;
 
+use Budgetlens\BolRetailerApi\Exceptions\ValidationException;
 use Budgetlens\BolRetailerApi\Requests\ListProductsRequest;
 use Budgetlens\BolRetailerApi\Resources\Product;
 use Budgetlens\BolRetailerApi\Resources\ProductList;
@@ -11,32 +12,30 @@ class ProductsTest extends TestCase
     /** @test */
     public function getProductsListWithFilterRange()
     {
-//        $this->useMock('200-list-products-by-category.json');
+        $this->useMock('200-list-products-filter-range.json');
 
-        $request = new ListProductsRequest([
+        $request = (new ListProductsRequest([
             'countryCode' => 'BE',
             'categoryId' => 38386,
             'searchTerm' => 'STICKERS',
             'filterRanges' => [
-                'rangeId' => 'RATING',
-                'min' => 1,
-                'max' => 4
+                [
+                    'rangeId' => 'RATING',
+                    'min' => 1,
+                    'max' => 4
+                ]
             ],
-        ]);
-
-        print_r($request);
-        exit;
+        ]))->addHeader('Accept-Language', 'fr-BE');
 
         $result = $this->client->products->list($request);
-
         $this->assertInstanceOf(ProductList::class, $result);
         $this->assertCount(2, $result->products);
         $this->assertInstanceOf(Product::class, $result->products->first());
-        $this->assertCount(2, $result->products->first()->eans);
-        $this->assertSame("Robotime modelbouw Miniatuur bouwpakket Cathy's Flower House hout/papier/kunststof - 195mm hoog x 175mm breed x 175mm diep - met lampje", $result->products->first()->title);
-        $this->assertSame('8718274546071', $result->products->first()->eans->get(0));
-        $this->assertSame('6946785108736', $result->products->first()->eans->get(1));
-        $this->assertSame('RELEVANCE', $result->sort);
+        $this->assertCount(1, $result->products->first()->eans);
+        $this->assertSame("Fille avec ballon, autocollant de voiture", $result->products->first()->title);
+        $this->assertSame('9343929000678', $result->products->first()->eans->get(0));
+        $this->assertSame('8020912014300', $result->products->last()->eans->get(0));
+        $this->assertSame('POPULARITY', $result->sort);
     }
 
     /** @test */
