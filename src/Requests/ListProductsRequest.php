c5146f0862374ac705f80915de2ced924268171b
Sebastiaan Blaas <sebastiaan@avido.nl>
2023-07-25 15:02:34 +0200

    Add support for filterRanges attribute and toArray method

--- src/Requests/ListProductsRequest.php
+++ src/Requests/ListProductsRequest.php
@@ -2,19 +2,45 @@
 
 namespace Budgetlens\BolRetailerApi\Requests;
 
-use Budgetlens\BolRetailerApi\Resources\Concerns\HasAttributes;
+use Budgetlens\BolRetailerApi\Requests\Support\FilterRange;
+use Budgetlens\BolRetailerApi\Support\Arr;
 
 class ListProductsRequest extends BaseRequest
 {
-    use HasAttributes;
-
-    private null | string $searchTerm;
-    private null | string $categoryId;
-    private null | string $sort;
-    private array $filterRanges = [];
-    private array $filterValues = [];
-    private string $countryCode = "NL";
-    private int $page = 1;
+    protected null | string $searchTerm;
+    protected  null | string $categoryId;
+    protected  null | string $sort;
+    protected array $filterRanges = [];
+    protected array $filterValues = [];
+    protected  string $countryCode = "NL";
+    protected  int $page = 1;
 
 
+    public function setFilterRangesAttribute($value)
+    {
+        if (is_array($value) && !Arr::isAssoc($value)) {
+            foreach ($value as $filter) {
+                $this->addFilterRangesAttribute($filter);
+            }
+        }
+    }
+
+    public function addFilterRangesAttribute($value): self
+    {
+        $this->filterRanges[] = new FilterRange($value);
+
+        return $this;
+    }
+
+    public function toArray(): array
+    {
+        return collect(parent::toArray())
+            ->when(count($this->filterRanges), function ($collection) {
+                $filters = collect($this->filterRanges)->map(function ($item) {
+                    return $item->toArray();
+                });
+
+                return $collection->put('filterRanges', $filters->all());
+            })->all();
+    }
 }
