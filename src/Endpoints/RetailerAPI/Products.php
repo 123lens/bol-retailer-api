a10ce464d3a6de7c56ec4317a91dce52ef7068f1
Sebastiaan Blaas <sebastiaan@avido.nl>
2023-07-25 14:28:25 +0200

    Add Products endpoint and associated methods for listing and retrieving products

---
+++ src/Endpoints/RetailerAPI/Products.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Budgetlens\BolRetailerApi\Endpoints\RetailerAPI;
+
+use Budgetlens\BolRetailerApi\Endpoints\BaseEndpoint;
+use Budgetlens\BolRetailerApi\Requests\Products\ListProductsRequest;
+use Budgetlens\BolRetailerApi\Resources\ProductList;
+use Budgetlens\BolRetailerApi\Resources\Retailer;
+use Illuminate\Support\Collection;
+
+class Products extends BaseEndpoint
+{
+
+    public function list(ListProductsRequest $request)
+    {
+        $response = $this->performApiCall(
+            'POST',
+            "products/list",
+            $request->toJson()
+        );
+
+        $result = $response ?? null;
+
+        if (!is_null($result)) {
+            return new ProductList($result);
+        }
+
+        return null;
+    }
+
+    public function get(
+        string | int $retailerId,
+    ): Retailer {
+        $response = $this->performApiCall(
+            'GET',
+            "retailers/{$retailerId}"
+        );
+        return new Retailer(collect($response));
+    }
+}
