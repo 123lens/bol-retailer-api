9e20f5268b15ab5293789640041c7f176c999308
Sebastiaan Blaas <sebastiaan@avido.nl>
2023-07-26 09:17:38 +0200

    Add support for listFilters to Products endpoint

--- src/Endpoints/RetailerAPI/Products.php
+++ src/Endpoints/RetailerAPI/Products.php
@@ -4,8 +4,10 @@ namespace Budgetlens\BolRetailerApi\Endpoints\RetailerAPI;
 
 use Budgetlens\BolRetailerApi\Endpoints\BaseEndpoint;
 use Budgetlens\BolRetailerApi\Requests\ListProductsRequest;
+use Budgetlens\BolRetailerApi\Resources\FiltersList;
 use Budgetlens\BolRetailerApi\Resources\ProductList;
 use Budgetlens\BolRetailerApi\Resources\Retailer;
+use Budgetlens\BolRetailerApi\Support\Str;
 
 class Products extends BaseEndpoint
 {
@@ -23,6 +25,32 @@ class Products extends BaseEndpoint
 
         if (!is_null($result)) {
             return new ProductList($result);
+        }
+
+        return null;
+    }
+
+    public function listFilters(ListProductsRequest $request)
+    {
+
+        $parameters = collect($request->toArray())
+            ->map(function ($data, $key) {
+                return [Str::snake($key, '-') => $data];
+            })
+            ->collapse()
+            ->reject(function ($value) {
+                return is_null($value);
+            })->all();
+
+        $response = $this->performApiCall(
+            'GET',
+            "products/list-filters" . $this->buildQueryString($parameters)
+        );
+
+        $result = $response ?? null;
+
+        if (!is_null($result)) {
+            return new FiltersList($result);
         }
 
         return null;
